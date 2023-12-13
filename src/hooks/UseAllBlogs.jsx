f9d030870973b34158839ce5bae70b8c67cb640f
Rakib <mohammadmohiuddin3490@gmai.com>
2023-12-13 14:00:22 +0600

    add

--- src/hooks/UseAllBlogs.jsx
+++ src/hooks/UseAllBlogs.jsx
@@ -3,19 +3,14 @@ import UseAxiosSecure from "./UseAxiosSecure";
 
 const UseAllBlogs = () => {
   const axiosSecure = UseAxiosSecure();
-  // const { user } = UseAuth();
   const {
     refetch,
     data: allBlogs = [],
     isPending: loading,
   } = useQuery({
-    // queryKey: ["adminAddBlog", user?.email],
     queryKey: ["adminAddBlog"],
     queryFn: async () => {
-      const res = await axiosSecure.get(
-        // `/adminAddBlog?email=${user.email}`
-        "/adminAddBlog"
-      );
+      const res = await axiosSecure.get("/adminAddBlog");
       return res.data;
     },
   });
